4790258008391ff3756a6e7ede0591ef642a2975
Vikas-Singh-Manav <109429394+Vikas-Singh-Manav@users.noreply.github.com>
2023-07-23 08:36:26 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is for making an notes</h1>
+      <p>
+        This can be very useful for making notes which will help us to remember
+        things fast as we can say it is a token to remember things.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
